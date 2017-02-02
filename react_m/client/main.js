ab3a5eaabc82990caed61350f76a9856bcf5b94f
MichaelWiss <michael.wiss@gmail.com>
2017-02-02 16:48:43 -0600

    main.html

--- react_m/client/main.js
+++ react_m/client/main.js
@@ -1,3 +1,12 @@
-import react from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = () => {
+   return (
+   	<h1>Hello there!</h1>
+   );
+};
+
+Meteor.startup(() => {
+	ReactDOM.render(<App />, document.querySelector('.render-target'));
+});
