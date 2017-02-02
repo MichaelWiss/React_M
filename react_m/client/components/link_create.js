9139fc684de7a6d390cb4585f56ea09cbc884e61
MichaelWiss <michael.wiss@gmail.com>
2017-02-02 17:27:01 -0600

    form button

--- react_m/client/components/link_create.js
+++ react_m/client/components/link_create.js
@@ -4,7 +4,11 @@ class LinkCreate extends Component {
 	render() {
 		return (
 			<form>
-			 
+			 <div className="form-group">
+				 <label>Link to shorten</label>
+				 <input className="form-control" />
+			 </div>
+			 <button className="btn btn-primary">Shorten!</button>
 			</form>
 	  );
 	}
