21f0129b21897d1e1dd0f4a8f6ae099ef6a8c4e4
MichaelWiss <michael.wiss@gmail.com>
2017-02-13 15:15:25 -0600

    token

--- react_m/imports/collections/links.js
+++ react_m/imports/collections/links.js
@@ -6,6 +6,9 @@ import { check, Match } from 'meteor/check';
 Meteor.methods({
 	'links.insert': function(url) {
 		check(url, Match.Where(url => validUrl.isUri(url)));
+
+
+		const token = Math.random().toString(36).slice(-5);
 	}
 });
 
