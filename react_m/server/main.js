8c8f34828a4ded7aab94ca844805dcee2fba4aff
MichaelWiss <michael.wiss@gmail.com>
2017-02-14 17:25:56 -0600

    link req redirect

--- react_m/server/main.js
+++ react_m/server/main.js
@@ -11,8 +11,27 @@ Meteor.startup(() => {
   });
 });
 
+//executed whenever a user visits with a route like
+//'localhost:3000/abcd'
+function onRoute(req, res, next) {
+	//take the token out of the url and try to find
+	//a matching link
+const link = Links.findOne({ token: req.params.token });
+
+if (link) {}
+	//if we find a link object, redirect the user to the 
+	//long url
+	res.writeHead(307, { 'Location': link.url });
+	res.end();
+} else {}
+
+	//if we don't find a link object, send the user
+	//to our normal React app
+  } 
+}
+
 const middleware = ConnectRoute(function(router) {
-    router.get('/:token', (req) => console.log(req));
+    router.get('/:token', onRoute);
 });
 
 WebApp.connectHandlers.use(middleware);
